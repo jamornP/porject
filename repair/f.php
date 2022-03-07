7848d89c1eb7e58e76febf25158eaf2735a346db
JamornPengsuay <75972115+JamornPengsuay@users.noreply.github.com>
2022-03-07 16:38:23 +0700

    sb admin

--- repair/f.php
+++ repair/f.php
@@ -3,6 +3,6 @@
 
 
 <script src="../theme/js/jquery.min.js"></script>
-<script src="../theme/js/dataTables.bootstrap4.min.js"></script>
+<script src="../theme/js/dataTables.bootstrap4.js"></script>
 <script src="../theme/js/jquery.dataTables.min.js"></script>
-<!-- <script src="../theme/js/datatables-demo.js"></script> -->
+<script src="../theme/js/datatables-demo.js"></script>
