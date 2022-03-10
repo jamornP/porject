809c450235330b2ca67f8bee051dc4a468cfe7aa
JamornPengsuay <75972115+JamornPengsuay@users.noreply.github.com>
2022-03-10 13:59:18 +0700

    d

--- repair/h.php
+++ repair/h.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ระบบแจ้งซ่อม</title>
-    <link rel="stylesheet" href="../theme/css/bootstrap-theme.css">
+    <!-- <link rel="stylesheet" href="../theme/css/bootstrap-theme.css"> -->
     <link rel="stylesheet" href="../theme/css/dataTables.bootstrap4.css">
     <link rel="stylesheet" href="../theme/css/sb-admin.css">
     
